eb426a4eba400a1bac889ef29237e8ec41cdd03e
Asrinsamad <asrin@baimaas.com>
2020-10-17 17:41:24 +0530

    Adding maven project

---
+++ src/test/java/com/asrin/maventest.java
@@ -0,0 +1,14 @@
+package com.asrin;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class maventest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
